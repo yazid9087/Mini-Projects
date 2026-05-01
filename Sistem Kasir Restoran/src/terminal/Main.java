9d02a1846f7a91a345e68e62cb45e124e8f6ccc9
Yazid <muhammadyazid9087@gmail.com>
2026-05-01 13:46:49 +0700

    Progress

--- Sistem Kasir Restoran/src/terminal/Main.java
+++ Sistem Kasir Restoran/src/terminal/Main.java
@@ -2,12 +2,18 @@ package terminal;
 
 public class Main {
     public static void main(String[] args) {
-        NotaPesanan notaBudi = new NotaPesanan("Budi", 5);
-        
-        notaBudi.tambahMenu(new Makanan("Sate Ayam", 20000, true)); 
-        notaBudi.tambahMenu(new Minuman("Es Jeruk", 8000, false)); 
-        notaBudi.tambahMenu(new Minuman("Kopi Susu", 15000, true)); 
+        NotaPesanan notaBudi = new NotaPesanan("Budi", 3);
+
+        notaBudi.tambahMenu(new Makanan("Sate Ayam", 20000, true));
+        notaBudi.tambahMenu(new Minuman("Es Jeruk", 8000, false));
+        notaBudi.tambahMenu(new Minuman("Kopi Susu", 15000, true));
 
         notaBudi.cetakStruk();
+
+        // Kita pakai Polimorfisme: Wadah Induk (Menu), isinya Anak (Makanan)
+        Menu tesPesanan = new Makanan("Sate Ayam", 20000, true);
+
+        // Perhatikan: Kita langsung mencetak objeknya, BUKAN memanggil .show()
+        System.out.println(tesPesanan);
     }
 }
