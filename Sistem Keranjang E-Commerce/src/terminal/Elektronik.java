fec7975f1d0361edf83772c901902fe91a935c72
Yazid <muhammadyazid9087@gmail.com>
2026-05-03 23:07:12 +0700

    Update

--- Sistem Keranjang E-Commerce/src/terminal/Elektronik.java
+++ Sistem Keranjang E-Commerce/src/terminal/Elektronik.java
@@ -1,5 +1,16 @@
 package terminal;
 
-public class Elektronik {
-    
+class Elektronik extends Barang{
+    private boolean asuransiPengiriman;
+
+    Elektronik(String namabarang, double hargaDasar, int beratGram, boolean asuransiPengiriman){
+        super(namabarang, hargaDasar, beratGram);
+        this.asuransiPengiriman = asuransiPengiriman;
+    }
+
+    @Override
+    public hargaAkhir(){
+        double asuransi = this.asuransiPengiriman ? (this.hargaDasar (this.hargaDasar * 0.05)) : 0;
+        return super.hargaDasar();
+    }
 }
