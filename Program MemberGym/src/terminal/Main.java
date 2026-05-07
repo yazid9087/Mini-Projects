fdd25ba3f5fe5babfa5dfa35a4b3bc7c6ae1e4ce
Yazid <muhammadyazid9087@gmail.com>
2026-05-07 14:10:29 +0700

    Compelete

--- Program MemberGym/src/terminal/Main.java
+++ Program MemberGym/src/terminal/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String[] args){
         var m1 = new memberGym("Anton", 12, 4, true);
         System.out.println(m1);
-        //iyakahh
+
         var m2 = new memberGym("Ariel", 24, 3, false);
         System.out.println(m2);
     }
